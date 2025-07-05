e3fdcc124cd7ef0543606f15ef3888154cbf9550
mielindev <phamvietquang.dev@gmail.com>
2025-07-05 22:05:18 +0700

    Refine Login and Logout function in authController

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,13 +1,102 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import generateToken from "../utils/jwt.js";
 const authController = {
-  signup: (req, res) => {
-    res.send("signup route");
+  // Signup route handler function
+  signup: async (req, res) => {
+    // Get user data
+    const { email, fullName, password } = req.body;
+    try {
+      if (!email || !fullName || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+      }
+      if (password.length < 6) {
+        return res
+          .status(400)
+          .json({ message: "Password must be at least 6 characters" });
+      }
+
+      const user = await User.findOne({ email });
+
+      if (user) return res.status(400).json({ message: "User already exists" });
+
+      const salt = bcrypt.genSaltSync(10);
+      const hashedPassword = bcrypt.hashSync(password, salt);
+
+      const newUser = new User({
+        email,
+        fullName,
+        password: hashedPassword,
+      });
+
+      if (newUser) {
+        // Generate token
+        generateToken(newUser._id, res);
+        await newUser.save();
+
+        return res.status(200).json({
+          _id: newUser._id,
+          email: newUser.email,
+          fullName: newUser.fullName,
+          profilePic: newUser.profilePic,
+        });
+      } else {
+        return res.status(400).json({ message: "Invalid user data" });
+      }
+    } catch (error) {
+      console.log("Error in signup", error);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
   },
-  login: (req, res) => {
-    res.send("login route");
+  // Login route handler function
+  login: async (req, res) => {
+    const { password, email } = req.body;
+    try {
+      if (!email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+      }
+
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ message: "Your email or password is incorrect" });
+      }
+
+      const isValidPassword = bcrypt.compareSync(password, user.password);
+
+      if (!isValidPassword) {
+        return res
+          .status(400)
+          .json({ message: "Your email or password is incorrect" });
+      }
+
+      // Generate token
+      generateToken(user._id, res);
+      return res.status(200).json({
+        _id: user._id,
+        email: user.email,
+        fullName: user.fullName,
+        profilePic: user.profilePic,
+      });
+    } catch (error) {
+      console.log("Error in login controller", error);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
   },
   logout: (req, res) => {
-    res.send("logout route");
+    try {
+      res.cookie("jwt", "", {
+        maxAge: 0,
+      });
+      return res.status(200).json({ message: "Logout successful" });
+    } catch (error) {
+      console.log("Error in logout controller", error);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
   },
+  updateProfile: async (req, res) => {},
 };
 
 export default authController;
---
+++ backend/src/utils/jwt.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  res.cookie("jwt", token, {
+    maxAge: 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV !== "development",
+  });
+  return token;
+};
+
+export default generateToken;
