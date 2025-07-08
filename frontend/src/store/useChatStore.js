9042b83b79e1ce34a2c3d337652950380dca76c4
mielindev <phamvietquang.dev@gmail.com>
2025-07-08 11:01:26 +0700

    feat: implement chat functionality with sidebar, chat container, and no chat selected components; add user management in store

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,39 @@
+import { create } from "zustand";
+import { toast } from "react-hot-toast";
+import { axiosInstance } from "../lib/axios";
+
+const useChatStore = create((set) => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUserLoading: true,
+  isMessageLoading: false,
+
+  getUsers: async () => {
+    set({ isUserLoading: true });
+    try {
+      const res = await axiosInstance.get("/message/users");
+      set({ users: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUserLoading: false });
+    }
+  },
+
+  getMessages: async (userId) => {
+    set({ isMessageLoading: true });
+    try {
+      const res = await axiosInstance.get(`/message/messages/${userId}`);
+      set({ messages: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isMessageLoading: false });
+    }
+  },
+
+  setSelectedUser: (selectedUser) => set({ selectedUser }),
+}));
+
+export default useChatStore;
