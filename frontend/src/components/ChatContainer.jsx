9e14fc6256b5b29590f1446055a5c0652cf06036
mielindev <phamvietquang.dev@gmail.com>
2025-07-10 11:24:35 +0700

    feat: Update ChatContainer component to display conversation starter
    * Update condition to display conversation starter when there are no messages
    * Add avatar and full name of selected user to conversation starter
    * Add lock icon to conversation starter
    * Improve styling of conversation starter

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -5,6 +5,7 @@ import MessageInput from "./MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
 import useAuthStore from "../store/useAuthStore";
 import { formatMessageTime } from "../utils/messageTime.util";
+import { Lock } from "lucide-react";
 
 const ChatContainer = () => {
   const {
@@ -12,7 +13,6 @@ const ChatContainer = () => {
     selectedUser,
     isMessageLoading,
     getMessages,
-    isSendingMessage,
     subscribeToMessages,
     unsubscribeFromMessages,
   } = useChatStore();
@@ -52,67 +52,91 @@ const ChatContainer = () => {
       <ChatHeader />
 
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
-        {messages.map((message, index) => {
-          const previousMessage = messages[index - 1];
-          const isSameSender = previousMessage?.senderId === message.senderId;
-          const isSameTime =
-            formatMessageTime(previousMessage?.createdAt) ===
-            formatMessageTime(message.createdAt);
+        {messages.length !== 0 ? (
+          messages.map((message, index) => {
+            const previousMessage = messages[index - 1];
+            const isSameSender = previousMessage?.senderId === message.senderId;
+            const isSameTime =
+              formatMessageTime(previousMessage?.createdAt) ===
+              formatMessageTime(message.createdAt);
 
-          const showHeader = !isSameSender || isSameTime;
+            const showHeader = !isSameSender || isSameTime;
 
-          const isLastMessage = index === messages.length - 1;
-          return (
-            <div
-              key={message._id}
-              className={`chat ${
-                message.senderId === authUser._id ? "chat-end" : "chat-start"
-              }`}
-              ref={isLastMessage ? bottomRef : null}
-            >
-              <div className="chat-image avatar">
-                <div className={`size-10 rounded-full border}`}>
-                  {(showHeader || isLastMessage) && (
-                    <img
-                      src={
-                        message.senderId === authUser._id
-                          ? authUser.profilePic || "./avatar.png"
-                          : selectedUser.profilePic || "./avatar.png"
-                      }
-                      alt={
-                        message.senderId === authUser._id
-                          ? authUser.fullName
-                          : selectedUser.fullName
-                      }
-                    />
-                  )}
+            const isLastMessage = index === messages.length - 1;
+            return (
+              <div
+                key={message._id}
+                className={`chat ${
+                  message.senderId === authUser._id ? "chat-end" : "chat-start"
+                }`}
+                ref={isLastMessage ? bottomRef : null}
+              >
+                <div className="chat-image avatar">
+                  <div className={`size-10 rounded-full border}`}>
+                    {(showHeader || isLastMessage) && (
+                      <img
+                        src={
+                          message.senderId === authUser._id
+                            ? authUser.profilePic || "./avatar.png"
+                            : selectedUser.profilePic || "./avatar.png"
+                        }
+                        alt={
+                          message.senderId === authUser._id
+                            ? authUser.fullName
+                            : selectedUser.fullName
+                        }
+                      />
+                    )}
+                  </div>
+                </div>
+
+                {showHeader && (
+                  <div className="chat-header">
+                    <time className="text-xs opacity-50 ml-1">
+                      {formatMessageTime(message.createdAt)}
+                    </time>
+                  </div>
+                )}
+
+                {message.image && (
+                  <img
+                    src={message.image}
+                    alt="Atthachment"
+                    className="w-full sm:max-w-[200px] rounded-md mb-2 border-none"
+                  />
+                )}
+
+                {message.text.length !== 0 && (
+                  <div className="chat-bubble">
+                    <p>{message.text}</p>
+                  </div>
+                )}
+              </div>
+            );
+          })
+        ) : (
+          <div className="flex flex-col items-center justify-between h-full">
+            <div className="">
+              <div className="avatar">
+                <div className="size-24 rounded-full relative">
+                  <img
+                    src={selectedUser.profilePic || "./avatar.png"}
+                    alt={selectedUser.fullName}
+                  />
                 </div>
               </div>
-
-              {showHeader && (
-                <div className="chat-header">
-                  <time className="text-xs opacity-50 ml-1">
-                    {formatMessageTime(message.createdAt)}
-                  </time>
-                </div>
-              )}
-
-              {message.image && (
-                <img
-                  src={message.image}
-                  alt="Atthachment"
-                  className="w-full sm:max-w-[200px] rounded-md mb-2 border-none"
-                />
-              )}
-
-              {message.text.length !== 0 && (
-                <div className="chat-bubble">
-                  <p>{message.text}</p>
-                </div>
-              )}
+              <h3 className="font-medium">{selectedUser.fullName}</h3>
             </div>
-          );
-        })}
+
+            <div className="flex justify-center items-center gap-2">
+              <p className="text-sm text-base-content/70">
+                Send a message to start a conversation with{" "}
+                {selectedUser.fullName}{" "}
+              </p>
+              <Lock size={14} />
+            </div>
+          </div>
+        )}
       </div>
 
       <MessageInput />
